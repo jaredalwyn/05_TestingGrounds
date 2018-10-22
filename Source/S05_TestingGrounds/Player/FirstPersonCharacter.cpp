abb473841c7457b43291309f110d0e23fac94c5d
Jared Alwyn <jaredalwyn22@gmail.com>
2018-10-22 13:04:16 -0500

    L.309

--- Source/S05_TestingGrounds/Player/FirstPersonCharacter.cpp
+++ Source/S05_TestingGrounds/Player/FirstPersonCharacter.cpp
@@ -83,7 +83,7 @@ void AFirstPersonCharacter::BeginPlay()
 	}
 	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
 	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
-	Gun->AnimInstance = Mesh1P->GetAnimInstance();
+	Gun->AnimInstance1P = Mesh1P->GetAnimInstance();
 
 	// Bind fire event
 	if (EnableTouchscreenMovement(InputComponent) == false)
